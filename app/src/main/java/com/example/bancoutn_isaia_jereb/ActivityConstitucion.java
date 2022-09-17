5cb9b53f0d7f8d3b3f34ea32b0953c7f3c1fd408
MateoJereb <mateojereb@gmail.com>
2022-09-17 10:57:20 -0300

    Layouts terminados

---
+++ app/src/main/java/com/example/bancoutn_isaia_jereb/ActivityConstitucion.java
@@ -0,0 +1,15 @@
+package com.example.bancoutn_isaia_jereb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityConstitucion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constitucion);
+        setTitle("Simular Plazo Fijo");
+    }
+}
--- app/src/main/java/com/example/bancoutn_isaia_jereb/MainActivity.java
+++ app/src/main/java/com/example/bancoutn_isaia_jereb/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bancoutn_isaia_jereb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Constituir Plazo Fijo");
+    }
+
+    public void onSimular(View vie){
+        Intent intent = new Intent(this,ActivityConstitucion.class);
+        startActivity(intent);
     }
 }
