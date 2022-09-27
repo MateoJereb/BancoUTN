8e54832f2eee6892db7ac883cb70368b65e973f2
MateoJereb <mateojereb@gmail.com>
2022-09-27 11:50:49 -0300

    Falta solo cambiar la moneda en la actividad de constitucion

--- app/src/main/java/com/example/bancoutn_isaia_jereb/ActivityConstitucion.java
+++ app/src/main/java/com/example/bancoutn_isaia_jereb/ActivityConstitucion.java
@@ -1,15 +1,151 @@
 package com.example.bancoutn_isaia_jereb;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+import com.example.bancoutn_isaia_jereb.databinding.ActivityConstitucionBinding;
+
+import org.w3c.dom.Text;
+
+import java.util.regex.Pattern;
 
 public class ActivityConstitucion extends AppCompatActivity {
+
+    private ActivityConstitucionBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_constitucion);
+
+        binding = ActivityConstitucionBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
         setTitle("Simular Plazo Fijo");
+
+        binding.diasSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int progress = seekBar.getProgress();
+                binding.diasTextView.setText(progress*30+" días");
+                calcular();
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
+        binding.renovacionCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                calcular();
+            }
+        });
+
+        TextWatcher watcherCalculos = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                calcular();
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        };
+
+        binding.tasaNomEditText.addTextChangedListener(watcherCalculos);
+        binding.tasaEfecEditText.addTextChangedListener(watcherCalculos);
+        binding.capitalEditText.addTextChangedListener(watcherCalculos);
+
+    }
+
+    private void calcular(){
+        if(camposCompletos()){
+            try{
+                double  tasaNom = Double.parseDouble(binding.tasaNomEditText.getText().toString()),
+                        tasaEfec = Double.parseDouble(binding.tasaEfecEditText.getText().toString()),
+                        capital = Double.parseDouble(binding.capitalEditText.getText().toString()),
+                        tasa;
+                int plazo = binding.diasSeekBar.getProgress();
+
+                boolean renovacionAuto = binding.renovacionCheckBox.isChecked();
+                if(renovacionAuto) tasa = tasaEfec;
+                else tasa = tasaNom;
+
+                double  intereses = capital*(tasa/100)*plazo/12,
+                        montoTotal = intereses+capital,
+                        montoAnual = capital*(1+(tasa/100));
+
+                intereses = Math.round(intereses*100.0)/100.0;
+                montoTotal = Math.round(montoTotal*100.0)/100.0;
+                montoAnual = Math.round(montoAnual*100.0)/100.0;
+
+                String  plazoStr = "Plazo: "+plazo*30+" días",
+                        capitalStr = "Capital: $"+capital,
+                        interesesStr = "Intereses ganados: $"+intereses,
+                        montoTotalStr = "Monto total: $"+montoTotal,
+                        montoAnualStr = "Monto anual: $"+montoAnual;
+
+                binding.plazoTextView.setText(plazoStr);
+                binding.capitalTextView.setText(capitalStr);
+                binding.interesesTextView.setText(interesesStr);
+                binding.montoTextView.setText(montoTotalStr);
+                binding.montoAnualTextView.setText(montoAnualStr);
+            }
+            catch(Exception e){ e.printStackTrace(); }
+        }
+    }
+
+    private boolean camposCompletos(){
+        if (binding.tasaNomEditText.getText().length() == 0) return false;
+        if (binding.tasaEfecEditText.getText().length() == 0) return false;
+        if (binding.capitalEditText.getText().length() == 0) return false;
+
+        return true;
+    }
+
+    public void onConfirmar(View view){
+        if(camposCompletos()){
+            if(binding.diasSeekBar.getProgress() != 0){
+                double capital = Double.parseDouble(binding.capitalEditText.getText().toString());
+                int plazo = binding.diasSeekBar.getProgress();
+
+                Intent intent = new Intent();
+                intent.putExtra("capital",capital);
+                intent.putExtra("plazo",plazo);
+                setResult(100,intent);
+                finish();
+            }
+            else{ //No indicó duración
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityConstitucion.this);
+                builder.setMessage("La duración no puede ser de 0 días.")
+                        .setNeutralButton("Aceptar",null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        }
+        else{ //Campos incompletos
+            AlertDialog.Builder builder = new AlertDialog.Builder(ActivityConstitucion.this);
+            builder.setMessage("Debe completar todos los campos.")
+                    .setNeutralButton("Aceptar", null);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
     }
 }
+
--- app/src/main/java/com/example/bancoutn_isaia_jereb/MainActivity.java
+++ app/src/main/java/com/example/bancoutn_isaia_jereb/MainActivity.java
@@ -1,22 +1,83 @@
 package com.example.bancoutn_isaia_jereb;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.bancoutn_isaia_jereb.databinding.ActivityConstitucionBinding;
+import com.example.bancoutn_isaia_jereb.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private double capital;
+    private int plazo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
         setTitle("Constituir Plazo Fijo");
+
+
     }
 
-    public void onSimular(View vie){
-        Intent intent = new Intent(this,ActivityConstitucion.class);
-        startActivity(intent);
+    public void onSimular(View view){
+        Intent intent = new Intent(this, ActivityConstitucion.class);
+        startActivityForResult(intent,111);
+    }
+
+    public void onConstituir(View view){
+        if(camposCompletos()){
+            String nombre = binding.nombreEditText.getText().toString();
+            String apellido = binding.apellidoEditText.getText().toString();
+            String moneda = binding.monedasSpinner.getSelectedItem().toString().toLowerCase();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Felicitaciones "+nombre+" "+apellido+"!")
+                    .setMessage("Tu plazo fijo de "+capital+" "+moneda+" por "+plazo*30+" días ha sido constituido!")
+                    .setNeutralButton("Aceptar",null);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else{ //Campos incompletos
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Indique nombre y apellido")
+                    .setNeutralButton("Aceptar",null);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == 100){
+            if(requestCode == 111){
+                binding.constituirButton.setEnabled(true);
+                capital = data.getExtras().getDouble("capital");
+                plazo = data.getExtras().getInt("plazo");
+            }
+        }
+    }
+
+    private boolean camposCompletos(){
+        if(binding.nombreEditText.getText().length() == 0) return false;
+        if(binding.apellidoEditText.getText().length() == 0) return false;
+        return true;
     }
 }
